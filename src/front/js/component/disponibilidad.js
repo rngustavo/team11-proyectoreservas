6fc130aeb2a37f6d4769b54bc1190b941a1da5a7
bramag94 <bramag94@hotmail.com>
2021-05-17 02:08:47 +0000

    cartas dan vuelta

--- src/front/js/component/disponibilidad.js
+++ src/front/js/component/disponibilidad.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
+import "../../styles/carta.scss";
 
 export const Disponibilidad = () => {
 	const { store, actions } = useContext(Context);
@@ -17,26 +18,36 @@ export const Disponibilidad = () => {
 
 	return (
 		<div className="container">
-			<nav className="nav nav-pills justify-content-center">
-				<a className="nav-item nav-link active" href="#clasesDisp" data-toggle="tab">
-					<i className="fas fa-calendar-alt" /> Clases Disponibles
-				</a>
-			</nav>
 			<div>
+				{" "}
+				<h1 className="titulo"> Clases Disponibles</h1>
+			</div>
+
+			<div className="card-deck">
 				{store.clasesdisponibles.map((classEl, index) => {
 					const f = new Date(classEl.FECHA_INICIO);
 					const date = f.getDate() + "/" + f.getMonth() + "/" + f.getFullYear();
 					const dia = nombreDelDiaSegunFecha(f);
 
 					return (
-						<div className="card" style={{ width: "18rem" }} key={index}>
-							<div className="card-body">
-								<h5 className="card-title">
-									{index} {classEl.NOMBRE}
-								</h5>
-								<p className="card-text">Fecha: {date}</p>
-								<p className="card-text">Fecha: {date}</p>
-								<p className="card-text">Espcacios: {classEl.ESPACIOS}</p>
+						<div className="carta-box" key={index}>
+							<div className="carta" style={{ width: "18rem" }}>
+								<div className="card-body cara">
+									<img
+										src="https://www.pulsorunner.com/wp-content/uploads/2014/10/default-img.gif"
+										className="card-img-top"
+										alt="..."
+									/>
+									<h1 className="card-title">{classEl.NOMBRE}</h1>
+								</div>
+								<div className="card-body cara detras">
+									<h1 className="card-title">{classEl.NOMBRE}</h1>
+									<p className="card-text">Descripción: {classEl.DESCRIPCION}</p>
+									<p className="card-text">Fecha: {date}</p>
+									<p className="card-text">Duración: {classEl.DURACION}</p>
+
+									<p className="card-text">Espacios: {classEl.ESPACIOS}</p>
+								</div>
 							</div>
 						</div>
 					);
